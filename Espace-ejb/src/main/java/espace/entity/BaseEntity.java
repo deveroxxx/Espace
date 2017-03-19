8428a309c80d92773e106feab090b0dc0da968db
Bakos János <devero@indamail.hu>
2017-03-19 20:58:53 +0100

    @Additions BaseEntity + user

---
+++ Espace-ejb/src/main/java/espace/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package espace.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" + "id=" + id + '}';
+    }
+}
---
+++ Espace-ejb/src/main/java/espace/entity/User.java
@@ -0,0 +1,39 @@
+package espace.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends BaseEntity {
+
+    private String realName;
+    private String loginName;
+    private String userName;
+
+
+
+
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
